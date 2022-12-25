20952ad0473b275902d3ce6fe30114ffcf5fa9e5
Jaromir Matas <mail@jaromir.net>
2022-12-25 17:36:16 +0100

    add argparse

--- pyskvorky.py
+++ pyskvorky.py
@@ -1,8 +1,34 @@
 from collections import Counter
 from copy import deepcopy, copy
 from curses.textpad import rectangle
+from argparse import ArgumentParser
 import curses
 import time
+
+
+## ARGPARSE part
+
+parser = ArgumentParser(description="Unlimited Tic-Tac-Toe is a grown-up version of the classic game played by two players on an infinite two-dimensional board. Players try to place 5 consecutive markers in a row (vertical, horizontal or diagonal). The game starts by placing a marker X on the square (0, 0) (the 'center' of the infinite board).", epilog="Enjoy!")
+
+parser.add_argument("-x", "--X_player", choices=["bot", "human"], default="bot",
+                    help="assign player to X marker")
+parser.add_argument("-o", "--O_player", choices=["bot", "human"], default="human",
+                    help="assign player to O marker")
+parser.add_argument("-r", "--reverse", action="store_true",
+                    help="reverse players; i.e. swap assignement to markers X and O")
+parser.add_argument("-d", "--debug", default=False, action="store_true",
+                    help="pause after each move")
+parser.add_argument("-s", "--sleep", default=0.1, type=float, action="store", metavar="seconds",
+                    help="run sleep timer after each move")
+parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+args = parser.parse_args()
+
+X_player = args.X_player if not args.reverse else args.O_player
+O_player = args.O_player if not args.reverse else args.X_player
+
+
+## GLOBALS part
 
 
 # constants:
@@ -25,26 +51,63 @@ buf = 4  # minimum empty space around the outermost marked fields, automatically
 minsize = 10  # minimum size of the initial playing field (square)
 
 
-def start_game():
-    global move, board, player_sym, opponent_sym, player_fun, opponent_fun, player_style, opponent_style
+## MODEL part
 
-    for _ in range(100):
-        draw_board()
-        player_sym, opponent_sym = opponent_sym, player_sym
-        player_fun, opponent_fun = opponent_fun, player_fun
-        player_style, opponent_style = opponent_style, player_style
-        board = board[::-1]
-        move = player_fun(move)
-        time.sleep(.1)
-        claimed, lost = board
-        if winning_lines():  # check for a winning move
-            draw_board()
-            screen.addstr(1, xoff, f"Well done, {player_sym}! Player {opponent_sym} crushed in {len(claimed)} moves.")
-            screen.addstr(2, xoff, f"Press any key to close the curses screen.")
-            break
 
+# ai bot player
 
 def bot(opponents_move):
+    # ai bot player
+    def score(move):
+        # create a copy of the board and open_lines, update the copies to reflect move, evaluate move
+        # in the new context and return without affecting the previous game state (i.e. globals)
+        def evaluate_board(marked, viewpoint):
+            # find valuable patterns, evaluate each of them and answer the total value of all patterns
+            # valuable patterns are potential winning lines with 2 to 4 player's symbols inside
+            # viewpoint allows to evaluate opponent's situation from the player's perspective
+            # use open_lines global to slightly optimize the evaluation
+
+            # to evaluate a board use a heuristic table of weights to value selected game patterns
+            # the tables are generated using the fibonacci sequence for simplicity and easy extendability for K > 5
+            value_table_opponent = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+            value_table_player = [0, 0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 144, 233, 377]
+            value_table = value_table_opponent if viewpoint=='opponent' else value_table_player
+
+            pattern_to_index = lambda pattern, count: ((K + 1) * 2 - len(pattern)) * (len(pattern) - 1) // 2 + count
+            # a heuristic formula converting a (pattern, count) pair to an index into a table of relative weights
+            # derived manually to provide somewhat satisfactory move evaluation results to beat a mediocre player
+            # Possible improvement: evaluate recursively for each of opponent's next resonable moves
+
+            c = Counter([line & marked for line in open_lines_])
+            e = [value_table[pattern_to_index(pattern, count)] for pattern, count in c.items() if len(pattern) > 1]
+
+            return sum(e)
+
+        claimed, lost = deepcopy(board)  # possibly optimize by copy(claimed) only, no deepcopy
+        open_lines_ = copy(open_lines)
+        # update simulated board state for move
+        claimed.add(move)
+        conflicting_lines = [line for line in envelope(move) if not set(line).isdisjoint(lost)]
+        open_lines_.update(envelope(move))
+        open_lines_.difference_update(conflicting_lines)
+        # evaluate the board from both players' point of view
+        player_score = evaluate_board(claimed, viewpoint='player')
+        opponent_score = evaluate_board(lost, viewpoint='opponent')
+
+        return player_score - opponent_score
+
+    def update_board(move, claimed, lost):
+        # use open_lines variable to optimize the computation a bit
+        # use conflicting_lines as a set of lines containing a mix of both player's symbols,
+        # thus no longer potentially winning lines
+
+        lost.add(move)
+        next_move_candidates.update(neighborhood(move))
+        next_move_candidates.difference_update(lost | claimed)
+        conflicting_lines = {line for line in envelope(move) if not set(line).isdisjoint(claimed)}
+        open_lines.update(envelope(move))
+        open_lines.difference_update(conflicting_lines)
+
     # update board status after opponent's move, select the best countermove and update board status accordingly
     # use next_move_candidates to collect reasonable candidates for the next move to be evaluated
     claimed, lost = board
@@ -55,66 +118,83 @@ def bot(opponents_move):
     return countermove
 
 
+## alternative ai bot player
+
+def bot2(opponents_move):
+    # alternative ai bot player
+    def score(move):
+        # create a copy of the board and open_lines, update the copies to reflect move, evaluate move
+        # in the new context and return without affecting the previous game state (i.e. globals)
+        def evaluate_board(marked, viewpoint):
+            # find valuable patterns, evaluate each of them and answer the total value of all patterns
+            # valuable patterns are potential winning lines with 2 to 4 player's symbols inside
+            # viewpoint allows to evaluate opponent's situation from the player's perspective
+            # use open_lines global to slightly optimize the evaluation
+
+            # to evaluate a board use a heuristic table of weights to value selected game patterns
+            # the tables are generated using the fibonacci sequence for simplicity and easy extendability for K > 5
+            value_table_opponent = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+            value_table_player = [0, 0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 144, 233, 377]
+            value_table = value_table_opponent if viewpoint=='opponent' else value_table_player
+
+            pattern_to_index = lambda pattern, count: ((K + 1) * 2 - len(pattern)) * (len(pattern) - 1) // 2 + count
+            # a heuristic formula converting a (pattern, count) pair to an index into a table of relative weights
+            # derived manually to provide somewhat satisfactory move evaluation results to beat a mediocre player
+            # Possible improvement: evaluate recursively for each of opponent's next resonable moves
+
+            c = Counter([line & marked for line in open_lines_])
+            e = [value_table[pattern_to_index(pattern, count)] for pattern, count in c.items() if len(pattern) > 1]
+
+            return sum(e)
+
+        claimed, lost = deepcopy(board)  # possibly optimize by copy(claimed) only, no deepcopy
+        open_lines_ = copy(open_lines)
+        # update simulated board state for move
+        claimed.add(move)
+        conflicting_lines = [line for line in envelope(move) if not set(line).isdisjoint(lost)]
+        open_lines_.update(envelope(move))
+        open_lines_.difference_update(conflicting_lines)
+        # evaluate the board from both players' point of view
+        player_score = evaluate_board(claimed, viewpoint='player')
+        opponent_score = evaluate_board(lost, viewpoint='opponent')
+
+        return player_score - opponent_score
+
+    def update_board(move, claimed, lost):
+        # use open_lines variable to optimize the computation a bit
+        # use conflicting_lines as a set of lines containing a mix of both player's symbols,
+        # thus no longer potentially winning lines
+
+        lost.add(move)
+        next_move_candidates.update(neighborhood(move))
+        next_move_candidates.difference_update(lost | claimed)
+        conflicting_lines = {line for line in envelope(move) if not set(line).isdisjoint(claimed)}
+        open_lines.update(envelope(move))
+        open_lines.difference_update(conflicting_lines)
+
+    # update board status after opponent's move, select the best countermove and update board status accordingly
+    # use next_move_candidates to collect reasonable candidates for the next move to be evaluated
+    claimed, lost = board
+    update_board(opponents_move, claimed, lost)
+    countermove = max(next_move_candidates, key=score)
+    update_board(countermove, lost, claimed)
+
+    return countermove
+
+
+# human player
+
 def human(opponents_move):
     # update board status after opponent's move, enter a countermove and update board status accordingly
     claimed, lost = board
-    update_board(opponents_move, claimed, lost)
+    lost.add(opponents_move)
     countermove = enter_move()
-    update_board(countermove, lost, claimed)
+    claimed.add(countermove)
     
     return countermove
 
 
-def update_board(move, claimed, lost):
-    # use open_lines variable to optimize the computation a bit
-    # use conflicting_lines as a set of lines containing a mix of both player's symbols,
-    # thus no longer potentially winning lines
-
-    lost.add(move)
-    next_move_candidates.update(neighborhood(move))
-    next_move_candidates.difference_update(lost | claimed)
-    conflicting_lines = {line for line in envelope(move) if not set(line).isdisjoint(claimed)}
-    open_lines.update(envelope(move))
-    open_lines.difference_update(conflicting_lines)
-
-
-def score(move):
-    # create a copy of the board and open_lines, update the copies to reflect move, evaluate move
-    # in the new context and return without affecting the previous game state (i.e. globals)
-
-    def evaluate_board(marked, viewpoint):
-        # find valuable patterns, evaluate each of them and answer the total value of all patterns
-        # valuable patterns are potential winning lines with 2 to 4 player's symbols inside
-        # viewpoint allows to evaluate opponent's situation from the player's perspective
-        # use open_lines global to slightly optimize the evaluation
-
-        # to evaluate a board use a heuristic table of weights to value selected game patterns
-        # the tables are generated using the fibonacci sequence for simplicity and easy extendability for K > 5
-        value_table_opponent = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-        value_table_player = [0, 0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 377]
-        value_table = value_table_opponent if viewpoint=='opponent' else value_table_player
-
-        pattern_to_index = lambda pattern, count: ((K + 1) * 2 - len(pattern)) * (len(pattern) - 1) // 2 + count
-        # a heuristic formula converting a (pattern, count) pair to an index into a table of relative weights
-        # derived manually to provide somewhat satisfactory move evaluation results to beat a mediocre player
-        # Possible improvement: evaluate two simulated moves ahead, instead of just one
-
-        c = Counter([line & marked for line in open_lines_])
-        e = [value_table[pattern_to_index(pattern, count)] for pattern, count in c.items() if len(pattern) > 1]
-        return sum(e)
-
-    claimed, lost = deepcopy(board)  # possibly optimize by copy(claimed) only, no deepcopy
-    open_lines_ = copy(open_lines)
-    # update simulated board state for move
-    claimed.add(move)
-    conflicting_lines = [line for line in envelope(move) if not set(line).isdisjoint(lost)]
-    open_lines_.update(envelope(move))
-    open_lines_.difference_update(conflicting_lines)
-    # evaluate the board from both players' point of view
-    player_score = evaluate_board(claimed, viewpoint='player')
-    opponent_score = evaluate_board(lost, viewpoint='opponent')
-    return player_score - opponent_score
-
+## helper functions
 
 def envelope(position=(0, 0), length=K):
     # return all k_lines containing the position with coordinates given in the position tuple
@@ -146,17 +226,12 @@ def neighborhood(position=(0, 0), radius=R):
     return neighborhood_
 
 
-def visible_playfield():
-    # Coordinates of the visible part of the board, i.e. upper left and bottom right corners.
-    # Note: these are relative to the board, not the curses screen or the rectangle playfield
-    ymin, xmin = (min(-minsize, min(i) - buf) for i in zip(*set.union(*board)))  # min coordinates
-    ymax, xmax = (max(minsize, max(i) + buf) for i in zip(*set.union(*board)))  # max coordinates
-    return ymin, xmin, ymax, xmax
-
-
 def winning_lines():
     claimed, _ = board
     return {line for line in envelope(move) if len(line & claimed) == K}
+
+
+## PRESENTER part
 
 
 def draw_board():
@@ -192,12 +267,12 @@ def draw_board():
 def enter_move():
     curses.noecho()
     screen.keypad(True)
+    ymin, xmin, ymax, xmax = visible_playfield()
     while True:
         y, x = screen.getyx()
-        ymin, xmin, ymax, xmax = visible_playfield()
         row, col = y - yoff + ymin, (x - xoff)//2 + xmin  # current position of the cursor on the playfield (not screen!)
         key = screen.getkey()
-        if key in ["x", "KEY_HOME"]:  # move the cursor to (0, 0) field
+        if key in ["f", "KEY_HOME"]:  # move the cursor to (0, 0) field
             screen.move(yoff - ymin, xoff - 2*xmin)
         elif key in ["r", "KEY_END"]:  # move the cursor to last move field
             screen.move(yoff + move[0] - ymin, xoff + 2*(move[1] - xmin))
@@ -215,7 +290,7 @@ def enter_move():
             screen.move(max(y-1, yoff + 1), min(x+2, xoff + 2*(xmax - xmin) - 2))
         elif key in ["z"]:  # move the cursor diagonally down & left
             screen.move(min(y+1, yoff + ymax - ymin -1), max(x-2, xoff + 2))
-        elif key in ["c"]:  # move the cursor diagonally down & right
+        elif key in ["x"]:  # move the cursor diagonally down & right
             screen.move(min(y+1, yoff + ymax - ymin -1), min(x+2, xoff + 2*(xmax - xmin) - 2))
         elif key in ["Q", chr(27)]:  # chr(27) == "KEY_ESCAPE"; quit the game
             raise KeyError('Game interrupted exception')
@@ -228,6 +303,37 @@ def enter_move():
     return row, col
 
 
+def visible_playfield():
+    # Coordinates of the visible part of the board, i.e. upper left and bottom right corners.
+    # Note: these are relative to the board, not the curses screen or the rectangle playfield
+    ymin, xmin = (min(-minsize, min(i) - buf) for i in zip(*set.union(*board)))  # min coordinates
+    ymax, xmax = (max(minsize, max(i) + buf) for i in zip(*set.union(*board)))  # max coordinates
+    return ymin, xmin, ymax, xmax
+
+
+## RUN THE APP part
+
+
+def start_game():
+    global move, board, player_sym, opponent_sym, player_fun, opponent_fun, player_style, opponent_style
+
+    for _ in range(200):
+        draw_board()
+        player_sym, opponent_sym = opponent_sym, player_sym
+        player_fun, opponent_fun = opponent_fun, player_fun
+        player_style, opponent_style = opponent_style, player_style
+        board = board[::-1]
+        move = player_fun(move)
+        #time.sleep(.1)  # pace a bot vs bot match
+        #screen.getch()  # step a bot vs bot match
+        claimed, _ = board
+        if winning_lines():  # check for a winning move
+            draw_board()
+            screen.addstr(1, xoff, f"Well done, {player_sym}! {opponent_sym} beaten up in {len(claimed)} moves.")
+            screen.addstr(2, xoff, f"Press any key to close the curses screen.")
+            break
+
+
 screen = curses.initscr()  # initialize the curses screen
 curses.start_color()  # initalize the default color set
 curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -235,8 +341,8 @@ curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
 winner_style = curses.color_pair(3) | curses.A_BOLD
 
-player_fun = bot  # first player, symbol 'X'
-opponent_fun = human  # second player, symbol 'O'
+player_fun = bot if X_player == 'bot' else human # first player, symbol 'X'
+opponent_fun = bot if O_player == 'bot' else human   # second player, symbol 'O'
 
 player_sym = 'X'
 opponent_sym = 'O'
