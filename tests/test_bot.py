92198b9f11c2ef9443394408ee4b888c89aff4fb
Jaromir Matas <mail@jaromir.net>
2023-01-07 19:17:49 +0100

    add tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -11,6 +11,7 @@ def _init_board():
     # Note: start the fixture name with '_' to prevent pylint warnings W0621 and W0623
     # this is a pytest/pylint deficiency; recommended fix is the '_' prefix or use of 'name' parameter; see:
     # https://stackoverflow.com/questions/46089480/pytest-fixtures-redefining-name-from-outer-scope-pylint
+
     bot.claimed, bot.lost = set(), set()
     bot.next_move_candidates = set()
     bot.open_lines = set()
@@ -19,31 +20,15 @@ def _init_board():
 @pytest.fixture
 def _prime_board():
     """Populate the board with 3 moves."""
+
     bot.claimed, bot.lost = {(0, 0), (0, 1), (0, 2)}, {(-1, 0), (-1, 2)}
-
-
-@pytest.mark.parametrize('move', [(-1, -2), (1, 1), (0, 0)], ids=str)
-def test_play_subsequent_move(move):
-    """Test play() returns a tuple of ints."""
-    countermove = bot.play(move)
-    row, col = countermove
-    assert isinstance(countermove, tuple)
-    assert isinstance(row, int)
-    assert isinstance(col, int)
-
-
-@pytest.mark.parametrize('move', [(-1, -2), (1, 1), (0, 0)], ids=str)
-def test_update_board(_init_board, move):
-    """Test update_board() updates the board with move and countermove."""
-    # Note: ignore open_lines and next_move_candidates for the moment
-    countermove = bot.play(move)
-    assert move in bot.lost
-    assert countermove in bot.claimed
 
 
 def test_globals():
     """Test globals are initialized to empty sets after import."""
-    # Note: reload the module to test the initialization status
+    # Note: we must reload the module to test the initialization status because if some other tests
+    # are run prior this one the status of the globals may chenge and the test would fail
+
     reload(bot)
     assert bot.claimed == set()
     assert bot.lost == set()
@@ -51,43 +36,102 @@ def test_globals():
     assert bot.open_lines == set()
 
 
+@pytest.mark.parametrize('move', [None, (-1, -2), (0, 0)], ids=str)
+def test_play(move):
+    """Test play() returns a 2-tuple of ints."""
+
+    countermove = bot.play(move)
+    assert isinstance(countermove, tuple)
+    assert len(countermove) == 2
+
+    # now that we know countermove is a 2-tuple, we can safely unpack it
+    row, col = countermove
+    assert isinstance(row, int)
+    assert isinstance(col, int)
+
+
+@pytest.mark.parametrize('move', [(-1, -2), (0, 0)], ids=str)
+def test_update_board(_init_board, move):
+    """Test update_board() updates the board with move and countermove."""
+    # Note: ignore open_lines and next_move_candidates for the moment
+
+    countermove = bot.play(move)
+    assert move in bot.lost
+    assert countermove in bot.claimed
+
+
 def test_initial_move(_init_board):
     """Test bot's initial move and the board after the move."""
+
     assert bot.play(None) == (0, 0)
     assert bot.claimed == {(0, 0)}
     assert bot.lost == set()
 
 
+# patterns, counts and expected results of pattern_to_index(pattern, count)
+# count indicates the number of occurences of the pattern in a potentially winning line;
+# note that the more occurences the less blocked and more valuable the pattern is
+# e.g. four positions pattern can occur either once in a line or twice depending on whether
+# both ends of the pattern on the board are free or just one end is free or whether there's
+# a free position inside the pattern, i.e. the pattern is not contiguous (see second item).
+# Note: the list doesn't exhaust all possible situations for single position patterns
+pattern_count_list = [  # pattern, count, expected result
+    [{(-5, -3), (-4, -2), (-3, -1), (-2, 0), (-1, 1)}, 1, 15],
+    [{(-5, -3), (-4, -2), (-3, -1), (-2, 0)},          2, 14],
+    [{(-5, -3), (-4, -2),           (-2, 0), (-1, 1)}, 1, 13],
+    [{(-5, -3), (-4, -2), (-3, -1)},                   3, 12],
+    [{(-5, -3), (-4, -2),           (-2, 0)},          2, 11],
+    [{(-5, -3),           (-3, -1),          (-1, 1)}, 1, 10],
+    [{(-5, -3), (-4, -2)},                             4, 9],
+    [{(-5, -3),           (-3, -1)},                   3, 8],
+    [{(-5, -3),                     (-2, 0)},          2, 7],
+    [{(-5, -3),                              (-1, 1)}, 1, 6],
+    [{(-5, -3)},                                       5, 5],
+    [{(-5, -3)},                                       1, 1],
+]
+
+
+@pytest.mark.parametrize('pattern, count, result', pattern_count_list, ids=str)
+def test_pattern_to_index(pattern, count, result):
+    """Test score() returns a number."""
+
+    assert bot.pattern_to_index(pattern, count) == result
+
+
 def test_score():
     """Test score() returns a number."""
+
     assert isinstance(bot.score((0, 0)), Number)
+
+
+# expected result of envelope((-1, 1), 5)
+env5 = [
+    frozenset({(-5, -3), (-4, -2), (-3, -1), (-2, 0), (-1, 1)}),
+    frozenset({(-4, -2), (-3, -1), (-2, 0), (-1, 1), (0, 2)}),
+    frozenset({(-3, -1), (-2, 0), (-1, 1), (0, 2), (1, 3)}),
+    frozenset({(-2, 0), (-1, 1), (0, 2), (1, 3), (2, 4)}),
+    frozenset({(-1, 1), (0, 2), (1, 3), (2, 4), (3, 5)}),
+    frozenset({(-5, 1), (-4, 1), (-3, 1), (-2, 1), (-1, 1)}),
+    frozenset({(-4, 1), (-3, 1), (-2, 1), (-1, 1), (0, 1)}),
+    frozenset({(-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1)}),
+    frozenset({(-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1)}),
+    frozenset({(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1)}),
+    frozenset({(-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1)}),
+    frozenset({(-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)}),
+    frozenset({(-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3)}),
+    frozenset({(-1, 0), (-1, 1), (-1, 2), (-1, 3), (-1, 4)}),
+    frozenset({(-1, 1), (-1, 2), (-1, 3), (-1, 4), (-1, 5)}),
+    frozenset({(-1, 1), (0, 0), (1, -1), (2, -2), (3, -3)}),
+    frozenset({(-2, 2), (-1, 1), (0, 0), (1, -1), (2, -2)}),
+    frozenset({(-3, 3), (-2, 2), (-1, 1), (0, 0), (1, -1)}),
+    frozenset({(-4, 4), (-3, 3), (-2, 2), (-1, 1), (0, 0)}),
+    frozenset({(-5, 5), (-4, 4), (-3, 3), (-2, 2), (-1, 1)})
+]
 
 
 def test_envelope():
     """Test envelope() for K=5"""
-    # expected result of envelope((-1, 1), 5)
-    env5 = [
-        frozenset({(-5, -3), (-4, -2), (-3, -1), (-2, 0), (-1, 1)}),
-        frozenset({(-4, -2), (-3, -1), (-2, 0), (-1, 1), (0, 2)}),
-        frozenset({(-3, -1), (-2, 0), (-1, 1), (0, 2), (1, 3)}),
-        frozenset({(-2, 0), (-1, 1), (0, 2), (1, 3), (2, 4)}),
-        frozenset({(-1, 1), (0, 2), (1, 3), (2, 4), (3, 5)}),
-        frozenset({(-5, 1), (-4, 1), (-3, 1), (-2, 1), (-1, 1)}),
-        frozenset({(-4, 1), (-3, 1), (-2, 1), (-1, 1), (0, 1)}),
-        frozenset({(-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1)}),
-        frozenset({(-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1)}),
-        frozenset({(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1)}),
-        frozenset({(-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1)}),
-        frozenset({(-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)}),
-        frozenset({(-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3)}),
-        frozenset({(-1, 0), (-1, 1), (-1, 2), (-1, 3), (-1, 4)}),
-        frozenset({(-1, 1), (-1, 2), (-1, 3), (-1, 4), (-1, 5)}),
-        frozenset({(-1, 1), (0, 0), (1, -1), (2, -2), (3, -3)}),
-        frozenset({(-2, 2), (-1, 1), (0, 0), (1, -1), (2, -2)}),
-        frozenset({(-3, 3), (-2, 2), (-1, 1), (0, 0), (1, -1)}),
-        frozenset({(-4, 4), (-3, 3), (-2, 2), (-1, 1), (0, 0)}),
-        frozenset({(-5, 5), (-4, 4), (-3, 3), (-2, 2), (-1, 1)})
-    ]
+
     assert bot.envelope((-1, 1), 5) == env5
 
 
@@ -109,6 +153,7 @@ nbr = {
 @pytest.mark.parametrize('position, radius', [((-1, 1), 1), ((-1, 1), 2), ((-1, 1), 3)], ids=str)
 def test_neighborhood_initial(_init_board, position, radius):
     """Test neighborhood() function limits the radius in the first move to 1."""
+
     assert bot.neighborhood(position, radius) == nbr[1]
 
 
@@ -116,4 +161,5 @@ def test_neighborhood_initial(_init_board, position, radius):
 def test_neighborhood_full(_prime_board, position, radius):
     """Test neighborhood() function won't limit the radius after the initial few (three) moves."""
     # Note: use prime_board fixture to populate the board to avoid automatic radius adjustment
+
     assert bot.neighborhood(position, radius) == nbr[radius]
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,49 +1,125 @@
-"""Tests for pyskvorky.helper module."""
-from pyskvorky import helper
+"""Tests for helper and cli modules."""
+from unittest.mock import patch
+import pytest
+from pyskvorky import helper, cli
+
+
+argv_list = [  # argv, result
+    (['pyskvorky'], ('bot', 'human', 0, False)),
+    (['pyskvorky', '-r'], ('human', 'bot', 0, False)),
+    (['pyskvorky', '-o', 'rob', '-d'], ('bot', 'rob', 0, True)),
+    (['pyskvorky', '-d'], ('bot', 'human', 0, False)),
+    (['pyskvorky', '-o', 'rob', '-s', '1'], ('bot', 'rob', 1, False)),
+    (['pyskvorky', '-orob', '-s', '1'], ('bot', 'rob', 1, False)),
+    (['pyskvorky', '-orob', '-s1'], ('bot', 'rob', 1, False)),
+    (['pyskvorky', '-obot'], ('bot', 'bot', 0, False)),
+    (['pyskvorky', '-oh'], ('bot', 'h', 0, False)),
+]
+
+
+@pytest.mark.parametrize('argv, result', argv_list, ids=str)
+def test_get_cli_args(argv, result):
+    """Test parsing correct cli arguments."""
+    # Note: let's use mocking instead of setting sys.argv directly
+    # https://stackoverflow.com/questions/18668947/how-do-i-set-sys-argv-so-i-can-unit-test-it
+    # this is what works too but we're trying to avoid:
+    # sys.argv = ['pyskvorky'] + []
+    # assert cli.get_cli_args() == ('bot', 'human', 0, False)
+    with patch('sys.argv', argv):
+        assert cli.get_cli_args() == result
+
+
+@pytest.mark.parametrize('argv', ['-h', '-v', '-w', '-o', '-x', '-vh', '-ho'], ids=str)
+def test_get_cli_args_SystemExit(argv):
+    """Test parsing incorrect cli arguments raises SystemExit error."""
+
+    with pytest.raises(SystemExit):
+        with patch('sys.argv', ['pyskvorky'] + [argv]):
+            cli.get_cli_args()
 
 
 def test_player():
     """Test Player class assigns a distinct mutable default value for each instance."""
     # assigning a mutable default value to an instance variable can be tricky; see:
     # https://stackoverflow.com/questions/2681243/how-should-i-declare-default-values-for-instance-variables-in-python
+
     dummy = "don't care value"
+    # initialize both player1 and player2 with an empty set of fields
     player1 = helper.Player(dummy, dummy, dummy)
     player2 = helper.Player(dummy, dummy, dummy)
     assert player1.fields == set()
     assert player2.fields == set()
 
-    # at this point both players still may be sharing the same object; let's find out:
+    # at this point both players may still be sharing the same object; let's find out:
     player1.fields.add((0, 0))
     assert player1.fields == {(0, 0)}
     assert player2.fields == set()
 
 
+def move_player_list():
+    """Return a list of arguments and results for test_winning_set"""
+
+    dummy = "don't care value"
+    # initialize player1 with a board containg a winning set of fields and player2 with a board without a winning set of fields
+    player1 = helper.Player(dummy, dummy, dummy, {(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1), (-3, 3), (-2, 2), (0, 0)})
+    player2 = helper.Player(dummy, dummy, dummy, {(-2, 1), (0, 1), (1, 1), (2, 1), (3, 1), (-3, 3), (-2, 2), (0, 0)})
+    winning_set = {(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1)}
+
+    return [(None, player1, frozenset()), ((-1, 1), player1, winning_set), ((-1, 1), player2, frozenset())]
+
+
+@pytest.mark.parametrize('move, player, result', move_player_list())
+def test_winning_set(move, player, result):
+    """Test winning_set() returns all fields forming a wining line"""
+
+    assert helper.winning_set(move, player) == result
+
+
+
+board_contents_list = [
+    # board_contents, result
+    (None, (-10, -10, 10, 10)),
+    ({(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1), (-3, 3)}, (-10, -10, 10, 10)),
+    ({(-9, -8), (-4, -2), (-3, -1), (-2, 0), (-1, 7)}, (-13, -12, 10, 11))
+]
+
+
+@pytest.mark.parametrize('board_contents, result', board_contents_list)
+def test_visible_playfield(board_contents, result):
+    """Test visible_playfield() returns the outer most coordinates of occupied fields plus buffer offset"""
+
+    assert helper.visible_playfield(board_contents) == result
+
+
+# expected result of envelope((-1, 1))
+env = [
+    frozenset({(-5, -3), (-4, -2), (-3, -1), (-2, 0), (-1, 1)}),
+    frozenset({(-4, -2), (-3, -1), (-2, 0), (-1, 1), (0, 2)}),
+    frozenset({(-3, -1), (-2, 0), (-1, 1), (0, 2), (1, 3)}),
+    frozenset({(-2, 0), (-1, 1), (0, 2), (1, 3), (2, 4)}),
+    frozenset({(-1, 1), (0, 2), (1, 3), (2, 4), (3, 5)}),
+    frozenset({(-5, 1), (-4, 1), (-3, 1), (-2, 1), (-1, 1)}),
+    frozenset({(-4, 1), (-3, 1), (-2, 1), (-1, 1), (0, 1)}),
+    frozenset({(-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1)}),
+    frozenset({(-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1)}),
+    frozenset({(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1)}),
+    frozenset({(-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1)}),
+    frozenset({(-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)}),
+    frozenset({(-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3)}),
+    frozenset({(-1, 0), (-1, 1), (-1, 2), (-1, 3), (-1, 4)}),
+    frozenset({(-1, 1), (-1, 2), (-1, 3), (-1, 4), (-1, 5)}),
+    frozenset({(-1, 1), (0, 0), (1, -1), (2, -2), (3, -3)}),
+    frozenset({(-2, 2), (-1, 1), (0, 0), (1, -1), (2, -2)}),
+    frozenset({(-3, 3), (-2, 2), (-1, 1), (0, 0), (1, -1)}),
+    frozenset({(-4, 4), (-3, 3), (-2, 2), (-1, 1), (0, 0)}),
+    frozenset({(-5, 5), (-4, 4), (-3, 3), (-2, 2), (-1, 1)})
+]
+
+
 def test_envelope():
-    """Test envelope() for K=5"""
+    """Test envelope() returns all 20 lines"""
     # Note: this is not a duplicate test to test_envelope in test_bot:
     # envelope() implementations in both bot.py and helper.py modules are independent
 
-    # expected result of envelope((-1, 1), 5)
-    env5 = [
-        frozenset({(-5, -3), (-4, -2), (-3, -1), (-2, 0), (-1, 1)}),
-        frozenset({(-4, -2), (-3, -1), (-2, 0), (-1, 1), (0, 2)}),
-        frozenset({(-3, -1), (-2, 0), (-1, 1), (0, 2), (1, 3)}),
-        frozenset({(-2, 0), (-1, 1), (0, 2), (1, 3), (2, 4)}),
-        frozenset({(-1, 1), (0, 2), (1, 3), (2, 4), (3, 5)}),
-        frozenset({(-5, 1), (-4, 1), (-3, 1), (-2, 1), (-1, 1)}),
-        frozenset({(-4, 1), (-3, 1), (-2, 1), (-1, 1), (0, 1)}),
-        frozenset({(-3, 1), (-2, 1), (-1, 1), (0, 1), (1, 1)}),
-        frozenset({(-2, 1), (-1, 1), (0, 1), (1, 1), (2, 1)}),
-        frozenset({(-1, 1), (0, 1), (1, 1), (2, 1), (3, 1)}),
-        frozenset({(-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1)}),
-        frozenset({(-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2)}),
-        frozenset({(-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3)}),
-        frozenset({(-1, 0), (-1, 1), (-1, 2), (-1, 3), (-1, 4)}),
-        frozenset({(-1, 1), (-1, 2), (-1, 3), (-1, 4), (-1, 5)}),
-        frozenset({(-1, 1), (0, 0), (1, -1), (2, -2), (3, -3)}),
-        frozenset({(-2, 2), (-1, 1), (0, 0), (1, -1), (2, -2)}),
-        frozenset({(-3, 3), (-2, 2), (-1, 1), (0, 0), (1, -1)}),
-        frozenset({(-4, 4), (-3, 3), (-2, 2), (-1, 1), (0, 0)}),
-        frozenset({(-5, 5), (-4, 4), (-3, 3), (-2, 2), (-1, 1)})
-    ]
-    assert helper.envelope((-1, 1), 5) == env5
+    assert helper.envelope((-1, 1)) == env
+    assert len(helper.envelope((-1, 1))) == 20
